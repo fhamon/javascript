41095f197856a783b5ce37857faaed384f5376c8
Frederick Hamon <frederick@deuxhuithuit.co>
2021-02-11 15:59:25 -0500

    fixed jshint errors

--- src/modules/ajax.js
+++ src/modules/ajax.js
@@ -9,12 +9,18 @@
 	const updatePage = (event) => {
 		let preventGoto = false;
 
+		if (!!event.data && !!event.data.mediator) {
+			return window.pd(event, true);
+		}
+
 		App.mediator.notify('url.changed', {event: event}, (key, ret) => {
 			preventGoto = ret;
 		});
 
 		if (!preventGoto) {
-			App.mediator.goto(document.location.href);
+			App.mediator.goto(document.location.href, undefined, {
+				type: event.type
+			}, false);
 		}
 
 		return window.pd(event, true);
